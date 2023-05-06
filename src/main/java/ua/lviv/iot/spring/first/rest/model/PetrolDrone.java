c1cbd1e6ab2d5bb058c9811b55c649a41f7aa414
serejek2004 <serejek2004@gmail.com>
2023-05-06 23:49:13 +0300

    Upload rest project

---
+++ src/main/java/ua/lviv/iot/spring/first/rest/model/PetrolDrone.java
@@ -0,0 +1,80 @@
+package ua.lviv.iot.spring.first.rest.model;
+
+import lombok.Getter;
+
+@Getter
+
+public final class PetrolDrone extends AbstractDrone {
+    private final double fuelCapacity;
+    private final double consumptionFuel;
+    private double currentMaxFlyingDistance;
+    private double currentFuelLevel;
+    private Integer Id;
+
+    public Integer setId(int i) {
+        Id = i;
+        return Id;
+    }
+
+    public PetrolDrone(final double fuelCapacity,
+                       final double currentFuelLevel,
+                       final double consumptionFuel,
+                       final double currentSpeed,
+                       final double currentAltitude) {
+        super(currentSpeed, currentAltitude);
+        this.fuelCapacity = fuelCapacity;
+        this.consumptionFuel = consumptionFuel;
+        this.currentFuelLevel = currentFuelLevel;
+    }
+
+    public double getCurrentFuelLevel() {
+        return currentFuelLevel;
+    }
+
+    public void refuel(final double amount) {
+        if (currentFuelLevel < fuelCapacity) {
+            currentFuelLevel += amount;
+        }
+        if (currentFuelLevel > fuelCapacity) {
+            currentFuelLevel -= amount;
+        }
+    }
+
+    public void useFuel(final double amount) {
+        if (currentFuelLevel > amount) {
+            currentFuelLevel -= amount;
+        }
+    }
+
+    @Override
+    public void flyAt(final double speedMetersPerMinute,
+                      final double altitude) {
+        setCurrentSpeed(speedMetersPerMinute);
+        setCurrentAltitude(altitude);
+    }
+
+    @Override
+    public void calculateMaxFlyingDistanceAtCurrentSpeed() {
+        final int formulaNumber = 100;
+        currentMaxFlyingDistance =
+                (currentFuelLevel / consumptionFuel)
+                        * formulaNumber;
+    }
+
+    @Override
+    public String getHeaders() {
+        return super.getHeaders()
+                + ",currentFuelLevel,fuelCapacity,consumptionFuel,currentMaxFlyingDistance";
+    }
+
+    @Override
+    public String toCSV() {
+        return super.toCSV()
+                + currentFuelLevel + ","
+                + fuelCapacity + ","
+                + consumptionFuel + ","
+                + currentMaxFlyingDistance;
+    }
+}
+
+
---
+++ src/main/java/ua/lviv/iot/spring/first/rest/writer/DroneWriter.java
@@ -0,0 +1,30 @@
+package ua.lviv.iot.spring.first.rest.writer;
+
+import ua.lviv.iot.spring.first.rest.model.AbstractDrone;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Comparator;
+import java.util.List;
+
+public class DroneWriter {
+    public void writeToFile(final List<AbstractDrone> droneList, final String fileName) throws FileNotFoundException {
+        PrintWriter printer = new PrintWriter(fileName);
+
+        droneList.sort(Comparator.comparing(o -> o.getClass().getName()));
+
+        String temporaryName = null;
+
+        for (AbstractDrone drone : droneList) {
+            if (temporaryName != drone.getClass().getName()) {
+                printer.println(drone.getClass().getSimpleName());
+                printer.println(drone.getHeaders());
+            }
+
+            printer.println(drone.toCSV());
+            temporaryName = drone.getClass().getName();
+
+        }
+        printer.close();
+    }
+}
